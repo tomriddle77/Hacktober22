a60a0a3982d534e7f85d6591f5ca42f63d055b85
Amulya Kumar Maurya <79034752+Amulya77@users.noreply.github.com>
2022-10-01 03:17:38 +0530

    Create Cost function patch 1.py (#4)

---
+++ Data Science Project/Cost function patch 1.py
@@ -0,0 +1,24 @@
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum = cost_sum + cost  
+    total_cost = (1 / (2 * m)) * cost_sum  
+
+    return total_cost
