c4dbc94907ab78d9996cf4f87c1d278d41a86750
SanBuilds <68767061+SanBuilds@users.noreply.github.com>
2022-10-01 13:32:14 +0530

    Create Type_of_Triangle.py
    Adding a basic python program to determine the type of triangle - equilateral, isosceles, scalene, given lengths of all the sides of a triangle.

---
+++ Python/Type_of_Triangle.py
@@ -0,0 +1,20 @@
+# Given length of all sides of a triangle determine the type of triangle - 
+# equilateral, isosceles or scalene, using if-else condition.
+
+# Input the length of 3 sides a, b, c of the triangle
+
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+
+# Equilateral Triangle: all sides a, b, c are equal i.e. a=b=c
+if a==b==c:
+    print("It is an Equilateral Triangle")
+
+# Isosceles Triangle: any two sides are equal a=b or b=c or c=a
+elif a==b or b==c or c==a:
+    print("It is an Isosceles Triangle")
+    
+# Scalene Triangle: if none of the sides are equal a!=b!=c
+else:
+    print("It is a Scalene Triangle")
