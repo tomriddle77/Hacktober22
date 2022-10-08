3044928aa74dc7472c0a86b01746d573661cf7fc
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2022-10-09 00:36:56 +0530

    Create bubblesort.cpp

---
+++ C++ algos/Sorting Algos/bubblesort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   bool swapped;
+   for (i = 0; i < n-1; i++)
+   {
+     swapped = false;
+     for (j = 0; j < n-i-1; j++)
+     {
+        if (arr[j] > arr[j+1])
+        {
+           swap(arr[j], arr[j+1]);
+           swapped = true;
+        }
+     }
+ 
+     // IF no two elements were swapped
+     // by inner loop, then break
+     if (swapped == false)
+        break;
+   }
+}
+ 
+// Function to print an array
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout <<" "<< arr[i];
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {5, 3, 1, 9, 8, 2, 4, 7};
+    int N = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, N);
+    cout <<"Sorted array: \n";
+    printArray(arr, N);
+    return 0;
+}
