28ea1ae0aa46e74d4b799504d8f3958fa5017d3e
Yashasvi <bhardwaj113yashasvi@gmail.com>
2022-10-31 23:36:20 +0530

    insertion sorting

--- C++ algos/Sorting Algos/InsertionSort.cpp
+++ C++ algos/Sorting Algos/InsertionSort.cpp
@@ -13,10 +13,6 @@ void insertionSort(int arr[], int n)
 		key = arr[i];
 		j = i - 1;
 
-		// Move elements of arr[0..i-1],
-		// that are greater than key, to one
-		// position ahead of their
-		// current position
 		while (j >= 0 && arr[j] > key)
 		{
 			arr[j + 1] = arr[j];
